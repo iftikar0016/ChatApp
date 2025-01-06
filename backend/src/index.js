820e4bfb10c8204b4cbcfebba62ee5bf6bff8deb
Iftikar Alam <iftikaralam0016@gmail.com>
2025-01-06 21:42:54 +0530

    Done with backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,26 +2,45 @@ import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
-const app = express();
+const __dirname = path.resolve();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     req.statusCode(statusCode).json({ message : err.message });
 })
 
-const port = 3000
+const PORT = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+server.listen(PORT, () => {
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+  });
